e804f11c06610ce4771356ceeab2795da266a52a
Alangkrita Sarkar <alangkritasarkar00@gmail.com>
2023-12-01 23:56:58 +0530

    modify header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/Header";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+        <Route path="/sign-up" element={<SignUp />} />
+      </Routes>
     </BrowserRouter>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,52 @@
 import React from "react";
-import { IoIosSearch } from "react-icons/io";
+import { LiaShoppingBagSolid } from "react-icons/lia";
+import { CiHeart } from "react-icons/ci";
+import { BsPerson } from "react-icons/bs";
+import { IoSearch } from "react-icons/io5";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
     <div className="bg-white shadow-md p-4 flex flex-row items-center justify-between">
-      <p className="font-bold sm:ml-10">
+      <Link to={"/"} className="font-bold sm:ml-10 sm:text-xl">
         <span className="text-pink-800">Weave</span>
-        <span>Whisper</span>
-      </p>
-      <div className="font-semibold flex gap-6">
-        <p className="hidden md:inline">Men</p>
-        <p className="hidden md:inline">women</p>
-        <p className="hidden md:inline">Kids</p>
+        <span className="text-slate-700">Whisper</span>
+      </Link>
+      <div className="font-bold flex gap-8 text-xs text-slate-700">
+        <p className="hidden md:inline hover:underline hover:underline-offset-8">
+          MEN
+        </p>
+        <p className="hidden md:inline hover:underline">WOMEN</p>
+        <p className="hidden md:inline hover:underline">KIDS</p>
       </div>
-      <form className="border p-2 focus-within:border-gray-500 md:w-80 flex items-center justify-between">
-        <input type="text" placeholder="Search..." className="outline-none" />
-        <IoIosSearch className="text-slate-400" />
+      <form className="px-4 p-2  md:w-[400px] flex items-center justify-between bg-gray-100 rounded-md focus-within:bg-transparent focus-within:border">
+        <input
+          type="text"
+          placeholder="Search..."
+          className="outline-none bg-transparent w-full"
+        />
+        <IoSearch className="text-slate-400 text-xl" />
       </form>
-      <ul className="flex gap-2 sm:mr-10">
-        <li>Signin</li>
-        <li>Wishlist</li>
-        <li>Bag</li>
-      </ul>
+      <div className="flex gap-6 sm:mr-10 text-center">
+        <Link
+          to={"sign-in"}
+          className="hover:underline hover:underline-offset-8"
+        >
+          <ul className=" flex flex-col items-center ">
+            <BsPerson className="text-2xl" />
+            <li className="text-xs font-semibold">Signin</li>
+          </ul>
+        </Link>
+
+        <ul className=" flex flex-col items-center">
+          <CiHeart className="text-2xl" />
+          <li className="text-xs font-semibold">Wishlist</li>
+        </ul>
+        <ul className=" flex flex-col items-center">
+          <LiaShoppingBagSolid className="text-2xl" />
+          <li className="text-xs font-semibold">Bag</li>
+        </ul>
+      </div>
     </div>
   );
 }
