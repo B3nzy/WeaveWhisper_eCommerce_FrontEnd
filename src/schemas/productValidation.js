384e2ec962b0c18ff61c79a8e9d46f1d35546b44
Sumit Mandal <sumit.mandal0123@gmail.com>
2023-12-28 14:57:49 +0530

    Modify some variable names.

--- src/schemas/productValidation.js
+++ src/schemas/productValidation.js
@@ -8,21 +8,21 @@ export const productSchema = yup.object().shape({
     .max(50, "*too long!")
     .required("*required"),
   description: yup.string().max(100, "*too long!").required("*required"),
-  actual_price: yup
+  actualPrice: yup
     .number()
     .positive()
     .min(1, "*product price should be minimum 1")
     .required("*required"),
-  selling_price: yup
+  sellingPrice: yup
     .number()
     .positive()
     .min(1, "*product price should be minimum 1")
     .max(
-      yup.ref("actual_price"),
-      "*selling price must be lesser than or equal to product actual price"
+      yup.ref("actualPrice"),
+      "*selling price must be lesser than or equal to actual product price"
     )
     .required("*required"),
-  inventory_count: yup
+  inventoryCount: yup
     .number()
     .positive()
     .min(1, "*add product")
@@ -45,9 +45,9 @@ export const productSchema = yup.object().shape({
     .required("*select one"),
   colors: yup.array().min(1, "choose at least one").required("*required"),
   sizes: yup.array().min(1, "choose at least one").required("*required"),
-  imageUrls: yup
-    .array()
-    .min(1, "choose at least one")
-    .max(4, "can upload max 4 images")
-    .required("*required"),
+  // imageUrls: yup
+  //   .array()
+  //   .min(1, "choose at least one")
+  //   .max(4, "can upload max 4 images")
+  //   .required("*required"),
 });
