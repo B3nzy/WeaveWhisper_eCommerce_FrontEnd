322c6c83972fa0352fdb4bc666e3cb8956ea2c70
Sumit Mandal <sumit.mandal0123@gmail.com>
2023-12-30 00:19:00 +0530

    add toast message when user registers.

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,6 +1,6 @@
 /* eslint-disable no-unused-vars */
-import React, { useState } from "react";
-import { Link, useNavigate } from "react-router-dom";
+import React, { useEffect, useRef, useState } from "react";
+import { Link, useLocation, useNavigate } from "react-router-dom";
 import { useFormik } from "formik";
 import axios from "axios";
 import { signInSchema } from "../schemas/signinValidation";
@@ -10,8 +10,12 @@ import {
   signInSuccess,
   signInFaliure,
 } from "../redux/user/userSlice";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function SignIn() {
+  const location = useLocation();
+  const toastEffectRan = useRef(false);
   const [loading, setLoading] = useState(false);
   const [userError, setUserError] = useState(null);
   const { currentUser } = useSelector((state) => state.user);
@@ -49,64 +53,92 @@ export default function SignIn() {
       validationSchema: signInSchema,
       onSubmit,
     });
+
+  const handleToast = () => {
+    toast.success("Success Notification !", {
+      position: toast.POSITION.BOTTOM_RIGHT,
+    });
+
+    console.log("test");
+  };
+
+  useEffect(() => {
+    if (
+      toastEffectRan.current === false &&
+      location.state != null &&
+      location.state.success === true &&
+      location.state.message !== null
+    ) {
+      toast.success(location.state.message);
+      return () => {
+        toastEffectRan.current = true;
+      };
+    }
+  }, [location.state]);
+
   return (
-    <div className="max-w-lg mx-auto items-center p-3">
-      <h1 className="my-7 font-semibold text-2xl text-center">Sign In</h1>
-      <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
-        <input
-          className={[
-            "border p-3 rounded-lg focus:outline-orange-200",
-            errors.email && touched.email && " border-red-600 border-[1.5px]",
-          ].join("")}
-          type="text"
-          placeholder="email"
-          id="email"
-          name="email"
-          value={values.email}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        {errors.email && touched.email && (
-          <p className="text-red-500 mt-[-12px] text-xs ml-2">{errors.email}</p>
+    <>
+      <ToastContainer />
+      <div className="max-w-lg mx-auto items-center p-3">
+        <h1 className="my-7 font-semibold text-2xl text-center">Sign In</h1>
+        <form className="flex flex-col gap-4" onSubmit={handleSubmit}>
+          <input
+            className={[
+              "border p-3 rounded-lg focus:outline-orange-200",
+              errors.email && touched.email && " border-red-600 border-[1.5px]",
+            ].join("")}
+            type="text"
+            placeholder="email"
+            id="email"
+            name="email"
+            value={values.email}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.email && touched.email && (
+            <p className="text-red-500 mt-[-12px] text-xs ml-2">
+              {errors.email}
+            </p>
+          )}
+          <input
+            className={[
+              "border p-3 rounded-lg focus:outline-orange-200",
+              errors.password &&
+                touched.password &&
+                " border-red-600 border-[1.5px]",
+            ].join("")}
+            type="password"
+            placeholder="password"
+            id="password"
+            name="password"
+            value={values.password}
+            onChange={handleChange}
+            onBlur={handleBlur}
+          />
+          {errors.password && touched.password && (
+            <p className="text-red-500 mt-[-12px] text-xs ml-2">
+              {errors.password}
+            </p>
+          )}
+          <button
+            type="submit"
+            onSubmit={handleSubmit}
+            disabled={loading}
+            className="bg-black text-white p-3 uppercase rounded-lg hover:opacity-90 disabled:opacity-80"
+          >
+            {loading ? "Loading..." : "Sign in"}
+          </button>
+        </form>
+        {userError && (
+          <p className="text-red-600 mt-1 text-xs ml-3">{userError}</p>
         )}
-        <input
-          className={[
-            "border p-3 rounded-lg focus:outline-orange-200",
-            errors.password &&
-              touched.password &&
-              " border-red-600 border-[1.5px]",
-          ].join("")}
-          type="password"
-          placeholder="password"
-          id="password"
-          name="password"
-          value={values.password}
-          onChange={handleChange}
-          onBlur={handleBlur}
-        />
-        {errors.password && touched.password && (
-          <p className="text-red-500 mt-[-12px] text-xs ml-2">
-            {errors.password}
-          </p>
-        )}
-        <button
-          type="submit"
-          onSubmit={handleSubmit}
-          disabled={loading}
-          className="bg-black text-white p-3 uppercase rounded-lg hover:opacity-90 disabled:opacity-80"
-        >
-          {loading ? "Loading..." : "Sign in"}
-        </button>
-      </form>
-      {userError && (
-        <p className="text-red-600 mt-1 text-xs ml-3">{userError}</p>
-      )}
-      <div className="flex gap-2 justify-center mt-5">
-        <p>Dont have an account?</p>
-        <Link to={"/sign-up"} className="text-green-700 hover:underline">
-          Sign Up
-        </Link>
+        <div className="flex gap-2 justify-center mt-5">
+          <p>Dont have an account?</p>
+          <Link to={"/sign-up"} className="text-green-700 hover:underline">
+            Sign Up
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
