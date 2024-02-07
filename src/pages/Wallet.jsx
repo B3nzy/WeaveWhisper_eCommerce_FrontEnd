1ba993a54ce565391008342fecc2b647543d24e8
Alangkrita Sarkar <alangkritasarkar00@gmail.com>
2024-02-07 23:03:16 +0530

    modify wallet page and add validations

--- src/pages/Wallet.jsx
+++ src/pages/Wallet.jsx
@@ -13,13 +13,24 @@ const razorpayKeyId = import.meta.env.VITE_RAZORPAY_KEY_ID;
 export default function Wallet() {
   const [balance, setBalance] = useState(0);
   const { currentUser } = useSelector((state) => state.user);
-  console.log(currentUser);
-
+  const [amountError, setAmountError] = useState(false);
   const [Razorpay] = useRazorpay();
 
   const [amount, setAmount] = useState();
 
   const handlePayment = async () => {
+    console.log(amount);
+    if (
+      amount === "" ||
+      amount === null ||
+      amount === undefined ||
+      amount < 1
+    ) {
+      toast.error("Please enter amount", {
+        position: toast.POSITION.TOP_RIGHT,
+      });
+      return;
+    }
     const res = await axios.post("/api/balance/addrequest", {
       id: currentUser.id,
       amount,
@@ -99,52 +110,60 @@ export default function Wallet() {
   return (
     <>
       <ToastContainer newestOnTop={true} className="top-16 w-fit" />
-      <div>
-        <div className="bg-blue-50 p-6 m-10 rounded-sm">
+      <div className="min-h-screen">
+        <div className="bg-blue-50 p-6 m-10 mt-20 rounded-sm ">
           <p className="text-blue-800 font-bold text-2xl flex items-center gap-3">
             <MdAccountBalanceWallet className="text-4xl" /> My Wallet
           </p>
           <hr />
-          <div className="flex w-full flex-row gap-6 my-6 mx-auto p-3 items-center ">
-            <div className="bg-blue-200 p-3 w-full flex flex-col h-56 items-center mx-auto justify-center gap-6">
+          <div className="flex w-full flex-col md:flex-row gap-6 my-6 mx-auto p-3 items-center ">
+            <div className="bg-green-100 p-3 w-full flex flex-col h-56 items-center mx-auto justify-center gap-6">
               <HiCurrencyRupee className="text-6xl text-green-700" />
-              <div className="capitalize p-3 bg-green-100 font-bold text-green-600 text-xl">
+              <div className="capitalize p-3 border-[3px] rounded-sm border-white font-bold text-green-600 text-xl">
                 Wallet balance : {balance}
               </div>
             </div>
             <div className="bg-blue-200 p-3 w-full flex flex-col h-56 items-center mx-auto justify-center gap-6">
               <label className="flex items-center flex-row flex-wrap gap-4">
-                <span className="capitalize p-3 font-bold text-blue-600 text-xl">
+                {/* <span className="capitalize p-3 font-bold text-blue-600 text-xl">
                   Add Amount{" "}
-                </span>
-                <input
-                  type="number"
-                  placeholder="amount..."
-                  value={amount}
-                  onChange={(e) => setAmount(e.target.value)}
-                  className="outline-none p-3  rounded-lg"
-                />
+                </span> */}
+                <div className="flex flex-row items-center gap-1 bg-white p-1 rounded-sm">
+                  <HiCurrencyRupee className="ml-2 text-xl text-slate-400" />
+                  <input
+                    type="text"
+                    placeholder="Enter amount"
+                    autoFocus
+                    value={amount}
+                    onChange={(e) => {
+                      setAmount(e.target.value);
+                      isFinite(e.target.value)
+                        ? setAmountError(false)
+                        : setAmountError(true);
+                    }}
+                    className="outline-none p-2  rounded-lg"
+                  />
+                </div>
               </label>
+              <p
+                className={[
+                  "text-xs text-red-500 mt-[-18px] ml-[-70px]",
+                  !amountError && " hidden",
+                ].join(" ")}
+              >
+                *Amount should be a number
+              </p>
+
               <button
+                disabled={amountError}
                 onClick={handlePayment}
-                className="uppercase p-3 font-bold text-white bg-blue-700 rounded-lg"
+                className="disabled:opacity-50 disabled:cursor-not-allowed disabled:bg-transparent disabled:text-blue-700 uppercase p-2 w-40 font-bold text-blue-700 border border-blue-700 hover:text-white hover:bg-blue-700 rounded-sm"
               >
                 Add Balance
               </button>
             </div>
           </div>
         </div>
-        {/* <div>Wallet balance : {balance}</div>
-      <label>
-        <span>Add custom amount : </span>
-        <input
-          type="number"
-          placeholder="amount..."
-          value={amount}
-          onChange={(e) => setAmount(e.target.value)}
-        />
-      </label>
-      <button onClick={handlePayment}>Add Balance</button> */}
       </div>
     </>
   );
